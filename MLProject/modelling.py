6293018793b3ec4e3889e22be08ccb5f9bbc1496
DianAzizah13 <126247109+DianAzizah13@users.noreply.github.com>
2025-06-30 10:11:12 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,32 +1,29 @@
 import mlflow
 import mlflow.sklearn
 import pandas as pd
+import os
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
 def main():
-    
     mlflow.set_experiment("Crop Recommendation - Random Forest")
+    mlflow.sklearn.autolog()
 
-    mlflow.sklearn.autolog(log_input_examples=True, log_model_signatures=True)
+    X_train = pd.read_csv("data_preprocessing/X_train.csv")
+    X_test = pd.read_csv("data_preprocessing/X_test.csv")
+    y_train = pd.read_csv("data_preprocessing/y_train.csv").values.ravel()
+    y_test = pd.read_csv("data_preprocessing/y_test.csv").values.ravel()
 
-    try:
-        X_train = pd.read_csv("data_preprocessing/X_train.csv")
-        X_test = pd.read_csv("data_preprocessing/X_test.csv")
-        y_train = pd.read_csv("data_preprocessing/y_train.csv").values.ravel()
-        y_test = pd.read_csv("data_preprocessing/y_test.csv").values.ravel()
-    except FileNotFoundError as e:
-        print(f"Error: {e}")
-        exit()
-    
     rf = RandomForestClassifier(random_state=42)
     rf.fit(X_train, y_train)
-    
+
     preds = rf.predict(X_test)
     acc = accuracy_score(y_test, preds)
+    print(f"Accuracy: {acc:.4f}")
 
-    print(f"[Without Tuning] Accuracy: {acc:.4f}")
-    print("\nMLflow autologging selesai.")
+    # Simpan model ke folder MLProject/model
+    os.makedirs("model", exist_ok=True)
+    mlflow.sklearn.save_model(rf, path="model")
 
 if __name__ == "__main__":
     main()
